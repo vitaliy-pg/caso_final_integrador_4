754ec86ac0af9c6bc5aa237fd11edf400fa0f1f8
vitaliy-pg <aljndrvt@myuax.com>
2024-04-06 14:23:14 +0200

    importo todas las librerias necesarias para el proyecto

--- src/main/java/casointegrador/EditorDeTextoInteractivo.java
+++ src/main/java/casointegrador/EditorDeTextoInteractivo.java
@@ -1,4 +1,29 @@
 package casointegrador;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.print.PrinterException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.Shape;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 
 public class EditorDeTextoInteractivo {
 }
